4f8c347ae688cad2835c05b9d36bc50519a1b751
AnshulDasyam <59790053+AnshulDasyam@users.noreply.github.com>
2020-01-12 16:32:44 +0530

    Revert "code"
    This reverts commit 3994900565f260f66bc252c27fb7119c3563f804.

--- Web Data/12...n.py
+++
@@ -1,9 +0,0 @@
-x = int(input())
-a = 0
-b = 0
-for y in range(1,x + 1) :
-    a +=1
-    if len(str(y)) >= 2 :
-        b *= 10**(len(str(y)) - 1)
-    b += y *10**(x  - a )
-print(b)
--- Web Data/ex 11.1.py
+++
@@ -1,8 +0,0 @@
-import re
-x = list()
-handle = open("mbox.txt")
-for lines in handle :
-    a = lines.rstrip()
-    if re.search('X-',a) :
-        x.append(re.findall('X-',a))
-print(len(x))
